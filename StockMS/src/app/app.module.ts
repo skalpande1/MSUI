78ab7a2c2eadfd09d29e276a9ef9e7668426b341
skalpande1 <shubhamkalpande1995@gmail.com>
2022-04-20 21:45:05 +0530

    componets added

--- StockMS/src/app/app.module.ts
+++ StockMS/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompanyComponent } from './components/company/company.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
--- StockMS/src/app/models/company.ts
+++ StockMS/src/app/models/company.ts
@@ -1,2 +1,15 @@
 export class Company {
+  constructor(
+    public id: number,
+    public name: string,
+    public open_rate: number,
+    public close_rate: number,
+    public peak_rate: number,
+    public least_rate: number,
+    public current_rate: number,
+    public market_cap_code: string,
+    public volume: number,
+    public stock_type: string,
+    public address: string
+  ) {}
 }
